0d4f3fc73c6c029d052ad2a511283a3d65b180de
eiscafewebsite-spec <eiscafewebsite@gmail.com>
2026-07-07 21:49:10 +0200

    Add files via upload

--- bilder-config.js
+++ bilder-config.js
@@ -4,22 +4,23 @@
 const BILDER = {
 
   titelbild: {
-    url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_001.jpg",
+    url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_001.gif",
     alt: "Eiscafé Nico"
   },
 
   eismobil: {
-    url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_002.jpg",
+    url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_002.gif",
     alt: "Das Eismobil von Eiscafé Nico"
   },
 
   galerie: [
-    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_003.jpg", titel: "Kleine Becher mit Erdbeeren" },
+    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_003.jpg", titel: "Kleiner Erdbeerbecher" },
     { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_004.jpg", titel: "Früchtebecher" },
     { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_005.jpg", titel: "Schokobächer" },
-    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_006.jpg", titel: "Latte Macchiato mit Verschiedene Sirup" },
+    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_006.jpg", titel: "Latte Macchiato Auch mit verschiedenen Sirup" },
     { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_007.jpg", titel: "Eiscafé" },
-    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_008.jpg", titel: "Erdbeere Quark Sahne Torte" },
+    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_009.jpg", titel: "Eis mit Obst" },
+    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_008.jpg", titel: "Erdbeer Quark Sahnetorte" },
   ]
 
 };
