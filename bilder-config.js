6882e169cde9e162b53d8bf722c843ddf457c443
eiscafewebsite-spec <eiscafewebsite@gmail.com>
2026-07-08 00:02:14 +0200

    Update bilder-config.js

--- bilder-config.js
+++ bilder-config.js
@@ -14,12 +14,6 @@ const BILDER = {
   },
 
   galerie: [
-    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_003.jpg", titel: "Kleine Becher mit Erdbeeren" },
-    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_004.jpg", titel: "Früchtebecher" },
-    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_005.jpg", titel: "Schokobächer" },
-    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_006.jpg", titel: "Latte Macchiato mit Verschiedene Sirup" },
-    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_007.jpg", titel: "Eiscafé" },
-    { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_008.jpg", titel: "Erdbeere Quark Sahne Torte" },
     { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_003.jpg", titel: "Latte Macchiato mit Verschiedene Geschmack" },
     { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_004.jpg", titel: "Früchtebecher" },
     { url: "https://raw.githubusercontent.com/ha7221483-oss/eiscafe-nico/main/bild_005.jpg", titel: "Schokobächer" },
