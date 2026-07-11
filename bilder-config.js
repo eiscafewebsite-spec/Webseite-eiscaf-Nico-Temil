199c6fcebc083b547e018427bf38b73c68f297c4
eiscafewebsite-spec <eiscafewebsite@gmail.com>
2026-07-12 00:32:56 +0200

    Add files via upload

---
+++ bilder-config.js
@@ -0,0 +1,19 @@
+// Diese Datei wurde automatisch von bilder_verwaltung.py erzeugt.
+// Bitte nicht von Hand bearbeiten -- einfach das Programm benutzen.
+
+const BILDER = {
+
+  titelbild: {
+    url: "https://raw.githubusercontent.com/NicoGalerie/Eislagengalerie/main/bild_001.jpg",
+    alt: "Eiscafé Nico"
+  },
+
+  eismobil: {
+    url: "https://raw.githubusercontent.com/NicoGalerie/Eislagengalerie/main/bild_002.jpg",
+    alt: "Das Eismobil von Eiscafé Nico"
+  },
+
+  galerie: [
+  ]
+
+};
